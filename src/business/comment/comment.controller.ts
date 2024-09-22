4bd699e14c2abfb5c5c5566eb372367b475dcdbe
worasalid <worasalid@gmail.com>
2024-09-22 20:56:01 +0700

    CRUD resource comment

---
+++ src/business/comment/comment.controller.ts
@@ -0,0 +1,68 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ReqCreateCommentDto, ReqEditCommentDto, ReqSearchCommentDto } from './dto/request';
+import { AuthUser } from 'src/common/decorator/user.decorator';
+import { IAuthUser } from '../auth/interfaces/auth.interface';
+import { PermissionGuard } from 'src/core/authorization/permission.guard';
+
+@ApiTags('Comment')
+@ApiBearerAuth()
+@Controller()
+export class CommentController {
+  constructor(private readonly commentService: CommentService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'comment list' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'ค้นหาข้อมูล Comment จาก postId',
+  })
+  @UsePipes(ValidationPipe)
+  findCommentList(@Query() params: ReqSearchCommentDto) {
+    return this.commentService.findCommentList(params);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'create comment' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'success',
+  })
+  @UsePipes(ValidationPipe)
+  createComment(@Body() params: ReqCreateCommentDto, @AuthUser() user: IAuthUser) {
+    return this.commentService.createComment(params, user);
+  }
+
+  @Put()
+  @ApiOperation({ summary: 'edit comment' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'success',
+  })
+  @UseGuards(PermissionGuard)
+  @UsePipes(ValidationPipe)
+  editComment(@Body() params: ReqEditCommentDto, @AuthUser() user: IAuthUser) {
+    return this.commentService.editComment(params, user);
+  }
+
+  @Delete(':commentId')
+  @ApiOperation({ summary: 'delete comment' })
+  @UseGuards(PermissionGuard)
+  deleteComment(@Param('commentId') commentId: number, @AuthUser() user: IAuthUser) {
+    return this.commentService.deleteComment(commentId, user);
+  }
+}
---
+++ src/business/comment/comment.module.ts
@@ -0,0 +1,21 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { CommentService } from './comment.service';
+import { CommentController } from './comment.controller';
+import { TransCommentRepository } from 'src/repositories/trans-comment.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransCommentEntity } from 'src/entities/trans-comment.entity';
+import { TransPostRepository } from 'src/repositories/trans-post.repository';
+import { TransPostEntity } from 'src/entities/trans-post.entity';
+import { AuthMiddleWare } from 'src/core/authentication/middleware/auth.middleware';
+import { JwtService } from '@nestjs/jwt';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([TransCommentEntity, TransPostEntity])],
+  controllers: [CommentController],
+  providers: [JwtService, CommentService, TransCommentRepository, TransPostRepository],
+})
+export class CommentModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleWare).forRoutes(CommentController);
+  }
+}
